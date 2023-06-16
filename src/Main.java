81fb0467a735f67f0fc3604b2c4ac58d68f93485
VincenzoTavassi <info@egadivacanze.it>
2023-06-16 15:11:00 +0200

    milestone 3

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@
 //ORDER BY country
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,15 +14,22 @@ public class Main {
         String username = "root";
         String password = "root";
 
+        System.out.println("Benvenuto, inserisci una parola da cercare");
+        Scanner scanner = new Scanner(System.in);
+        String userCountry = scanner.nextLine();
+        scanner.close();
+
         try (Connection con = DriverManager.getConnection(host, username, password)) {
             String query = """
 SELECT countries.country_id as country_id, countries.name as country, regions.name as region_name, continents.name as continent_name FROM `countries`
-JOIN regions ON regions.region_id = countries.country_id
+JOIN regions ON regions.region_id = countries.region_id
 JOIN continents ON continents.continent_id = regions.continent_id
+WHERE countries.name LIKE ?
 ORDER BY country;
           """;
 
             try (PreparedStatement ps = con.prepareStatement(query)) {
+                ps.setString(1, "%" + userCountry + "%");
                 try (ResultSet result = ps.executeQuery()) {
                     while(result.next()) {
                         System.out.println(result.getInt("country_id") + " " + result.getString("country") + " " +
